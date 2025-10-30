8c50edb17bc5f450d4c287bd6c4f0c3a107e499e
ehmedcavidli-lang <ehmedcavidli@gmail.com>
2025-10-30 13:33:57 +0400

    Products shows after login

---
+++ Seeing_products_after_Login/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seeing_products_after_Login.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public float Discount { get; set; }
+        public DateTime CreatedAt { get; set; }=DateTime.Now;
+        public DateTime DiscountExpired { get; set; } = DateTime.Now.AddDays(3);
+        public Product(int id,string name,float price,float discount)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Discount = discount;
+           
+
+        }
+     
+        public  void GetInfo()
+        {
+            Console.WriteLine($"Product ID : {Id}\nProduct Name: {Name}\nProduct " +
+                $"Price {Price}\nProduct " +
+                $"last price: " +
+                $"{Price*(100-Discount)/100}\nCreated Product :{CreatedAt}\n" +
+                $"Expired Discount:{DiscountExpired} ");
+        }
+    }
+    
+}
---
+++ Seeing_products_after_Login/Program.cs
@@ -0,0 +1,3 @@
+﻿using Seeing_products_after_Login.Models;
+
+Product product=new Product(1,"Laptop",15550.0f,10.0f);
