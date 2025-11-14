9cd21ec56d712c920aaf93456f7b2033794f11f7
ehmedcavidli-lang <ehmedcavidli@gmail.com>
2025-11-14 13:22:02 +0400

    Html first

--- Architecture/AyncArchitec/AyncArchitec/obj/Debug/net8.0/AyncArchitec.AssemblyInfo.cs
+++ Architecture/AyncArchitec/AyncArchitec/obj/Debug/net8.0/AyncArchitec.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AyncArchitec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4516a586e746b245eff6930058a9fc807c4098b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AyncArchitec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AyncArchitec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
